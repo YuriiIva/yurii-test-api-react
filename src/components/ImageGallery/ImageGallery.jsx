9942e2bc528318355e56a1ebbc73fc1a7b46c308
YurkaBest <Ivaschenko_u@ukr.net>
2021-12-12 22:55:06 +0200

    a

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ImageGallery = () => {
+  return (
+    <div>
+      <ul class="gallery">
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default ImageGallery;
